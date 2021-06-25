84166ff7fd131eecd155ef448373abf77772d122
Gal Silberman <gal123@gmail.com>
2021-06-25 19:31:17 +0300

    fix

--- zip.js
+++ zip.js
@@ -4,7 +4,7 @@ const glob = require("glob")
 
 const cwd = 'dist/'
 const folderPath = '**';
-const out = 'galsilb-button-panel.zip';
+const out = 'cloudspout-button-panel.zip';
 
 glob(folderPath, {cwd}, (err, files) => {
 
@@ -18,11 +18,11 @@ glob(folderPath, {cwd}, (err, files) => {
     archive.pipe(output);
 
     // Linux needs the root folder itself
-    archive.file(cwd, {name:'galsilb-button-panel/'})
+    archive.file(cwd, {name:'cloudspout-button-panel/'})
     files.forEach(file => {
         if (file === out) return;
         console.log(file);
-        archive.file(cwd + file, {name:'galsilb-button-panel/'+file});
+        archive.file(cwd + file, {name:'cloudspout-button-panel/'+file});
     });
 
     archive.finalize();
